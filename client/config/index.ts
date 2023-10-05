534449b4fdf234f3233a988f2c61c4da052b4ff0
Francisco <frandelosantoss12@gmail.com>
2023-10-05 17:45:29 -0300

    Fix client config

--- client/config/index.ts
+++ client/config/index.ts
@@ -1,6 +1,6 @@
 export default {
   APIURL: `${window.location.protocol}//${window.location.hostname}/api`,
-  webSocketAPIURL: `ws://${window.location.hostname}/api`,
+  webSocketAPIURL: `wss://${window.location.hostname}/api`,
   gitHubOAuthURL: `https://github.com/login/oauth/authorize?redirect_uri=${window.location.protocol}//${window.location.hostname}?provider=github`,
   gitHubClientID: import.meta.env.VITE_GITHUB_CLIENT_ID as string,
 }
