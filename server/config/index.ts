baa1105daa5d7c09db5b36beb6ee1ccb8dd2ca90
Frandols <frandelosantoss12@gmail.com>
2025-11-04 23:36:26 -0300

    Enable just client origin in CORS settings

--- server/config/index.ts
+++ server/config/index.ts
@@ -7,5 +7,6 @@ export default {
   gitHubClientSecret: String(process.env.GITHUB_CLIENT_SECRET),
   googleClientID: String(process.env.GOOGLE_CLIENT_ID),
   googleClientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-  jwtSecret: String(process.env.JWT_SECRET)
+  jwtSecret: String(process.env.JWT_SECRET),
+  clientOrigin: String(process.env.CLIENT_ORIGIN)
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: '*' })
+  app.enableCors({ origin: config.clientOrigin, credentials: true })
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api')
 
