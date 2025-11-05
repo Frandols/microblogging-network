161c14055e9e0db7c0aa9d30e2385f65baae2f5b
Francisco Nicolás De Los Santos <frandelosantoss12@gmail.com>
2025-11-04 18:03:32 -0800

    Remove CORS settings from main.ts
    Removed CORS configuration from the bootstrap function.

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: config.clientOrigin,
-    credentials: true
-  })
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api')
 
