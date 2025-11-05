b4996ef39314977441032e08fba575fc96e75288
Francisco Nicolás De Los Santos <frandelosantoss12@gmail.com>
2025-11-04 17:57:15 -0800

    Enhance CORS settings in main.ts
    Updated CORS configuration to allow specific origin and credentials.

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  app.enableCors({
+    origin: config.clientOrigin,
+    credentials: true
+  })
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api')
 
